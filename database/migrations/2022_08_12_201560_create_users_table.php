7dde72a28f81774b112177042fd904cbbbdaa572
Duwan Fortunato <duwancontacto@gmail.com>
2022-08-13 22:51:14 -0400

    First commit

--- database/migrations/2022_08_12_201560_create_users_table.php
+++ database/migrations/2022_08_12_201560_create_users_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('usersapp', function (Blueprint $table) {
+        Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('correo');
             $table->string('role');
@@ -43,6 +43,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('usersapp');
+        Schema::dropIfExists('users');
     }
 };
